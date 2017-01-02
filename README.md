f70e25d2972b5496072c1848e0594fdb7efa016a
Jason E. Aten <j.e.aten@gmail.com>
2017-01-02 09:20:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# package diff
+
+package diff is a set of utilities for comparing
+strings while ignore whitespace differences.
---
+++ diff.go
@@ -0,0 +1,49 @@
+/*
+package diff is a set of utilities for comparing
+strings while ignore whitespace differences.
+*/
+package diff
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func Diffb(a string, b string) []byte {
+
+	dirpath := NewSimpleTempDir("diffdir_")
+	defer os.RemoveAll(dirpath)
+
+	fa := SimpleTempFile(dirpath)
+	fmt.Fprintf(fa, "%s\n", a)
+	fa.Close()
+
+	fb := SimpleTempFile(dirpath)
+	fmt.Fprintf(fb, "%s\n", b)
+	fb.Close()
+
+	co, err := exec.Command("diff", "-b", fa.Name(), fb.Name()).CombinedOutput()
+	if err != nil {
+		// don't panic, diff returns 2 on differences
+	}
+	return co
+}
+
+func NewSimpleTempDir(prefix string) string {
+	dirpath, err := ioutil.TempDir(".", prefix)
+	if err != nil {
+		panic(err)
+	}
+	return dirpath
+}
+
+func SimpleTempFile(dirpath string) *os.File {
+
+	f, err := ioutil.TempFile(dirpath, "diff_file_")
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
